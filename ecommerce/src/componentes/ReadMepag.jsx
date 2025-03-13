e2b4299f9d687d29d42c589a10c2e1983744a546
HUNTER4FWD <jgarcia@fwdcostarica.com>
2025-03-13 13:12:45 -0600

    final

--- ecommerce/src/componentes/ReadMepag.jsx
+++ ecommerce/src/componentes/ReadMepag.jsx
@@ -60,8 +60,8 @@ function ReadMepag(){
 
                 Se utilizan tablas para mostrar la lista de trabajadores y paquetes en un formato organizado y manipulable (sortable).
                 Enlaces Importantes:
-                Trello: Tablero de Trello – Detalles sobre el progreso y tareas del proyecto.
-                GitHub: Repositorio de GitHub – Código fuente del proyecto.
+                Trello: Thttps://trello.com/invite/b/67d25db0c61b9d71fa08c881/ATTI4a37d7c2d96d88c145898ab9f0e6082aFECD40BA/todo-ecommerce
+                GitHub: https://github.com/Baironha/Ecommerce.git
                 Tecnologías Utilizadas:
                 Frontend: React.js para la creación de componentes y la gestión del estado de la aplicación.
                 Estilos: CSS.
